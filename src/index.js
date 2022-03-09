b7682921f006d4692c2148c2edbc450813153f05
joellesenne <jfkles@me.com>
2022-03-09 20:25:34 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.clear()
+
+	const consoleStyle = 'font-weight:bold;font-size:50px;line-height:2;background:hsl(256, 100%, 50%);color:hsl(33, 93%, 66%);font-family:monospace;'
+	console.log('%c Made by @joellesenne ', consoleStyle)
