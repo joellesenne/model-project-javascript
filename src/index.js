e691c06082075c7a6b58d6962c00fdb5c423264e
joellesenne <jfkles@me.com>
2022-03-11 18:56:37 +0100

    eslint config & modern reset CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-console.clear()
+window.onload = () => {
+  console.clear();
 
-	const consoleStyle = 'font-weight:bold;font-size:50px;line-height:2;background:hsl(256, 100%, 50%);color:hsl(33, 93%, 66%);font-family:monospace;'
-	console.log('%c Made by @joellesenne ', consoleStyle)
+	const consoleStyle = 'font-weight:bold;font-size:50px;line-height:2;background:hsl(256, 100%, 50%);color:hsl(33, 93%, 66%);font-family:monospace;';
+	console.log('%c Made by @joellesenne ', consoleStyle);
+};
+
+
